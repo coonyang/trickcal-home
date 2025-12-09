e736c62caa3ca8c4b026a626ed570d2511d38ccb
Hajun Jeon <gkwns1231@naver.com>
2025-12-09 19:56:44 +0900

    4page 전체 기반 잡기

--- src/4pages/page4.jsx
+++ src/4pages/page4.jsx
@@ -2,7 +2,32 @@ import "./page4.css";
 export default function Page4() {
   return (
     <div className="main-bg4">
-      <div></div>
+      <div className="char-list">
+        <ul className="list-img">
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+          <li>
+            <img src="/img/4page/character-01.png"></img>
+          </li>
+        </ul>
+        <img src="/img/4page/character-text.png"></img>
+      </div>
     </div>
   );
 }

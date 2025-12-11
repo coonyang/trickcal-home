a77d6533c9af3a1814aa81db5dd0342ca5e6dc93
Hajun Jeon <gkwns1231@naver.com>
2025-12-11 18:31:33 +0900

    4page 작업 완성

--- src/4pages/page4.jsx
+++ src/4pages/page4.jsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 import "./page4.css";
 export default function Page4() {
   const [isOpen, setIsOpen] = useState(false);
+  const [viewImg, setViewImg] = useState(1);
+  const [viewImgTab, setViewImgTab] = useState(1);
+
+  const showImg = `/img/4page/character-${viewImg
+    .toString()
+    .padStart(2, "0")}-${viewImgTab.toString().padStart(2, "0")}.png`;
+
+  const tabCount = {
+    1: 3,
+    2: 3,
+    3: 3,
+    4: 3,
+    5: 3,
+    6: 3,
+    7: 4,
+  };
 
   return (
     <div className="main-bg4">
@@ -12,6 +28,7 @@ export default function Page4() {
               key={i}
               onClick={() => {
                 setIsOpen(true);
+                setViewImg(i + 1);
               }}
             >
               <img
@@ -29,12 +46,22 @@ export default function Page4() {
         <div className="view">
           <ul>
             <li>
-              <img src="/img/4page/character-01-02.png"></img>
+              <img src={showImg}></img>
               <a className="sound"></a>
               <a className="change"></a>
-              <ol className="view-tab">
-                {Array.from({ length: 3 }).map((_, idx) => (
-                  <li key={idx} onClick={() => {}}></li>
+              <ol
+                className="view-tab"
+                style={{
+                  width: tabCount[viewImg] === 4 ? "25%" : "19%",
+                }}
+              >
+                {Array.from({ length: tabCount[viewImg] }).map((_, idx) => (
+                  <li
+                    key={idx}
+                    onClick={() => {
+                      setViewImgTab(idx + 1);
+                    }}
+                  ></li>
                 ))}
               </ol>
               <button
@@ -47,7 +74,12 @@ export default function Page4() {
           </ul>
           <ul className="view-mark">
             {Array.from({ length: 7 }).map((_, idx) => (
-              <li key={idx} onClick={() => {}}></li>
+              <li
+                key={idx}
+                onClick={() => {
+                  setViewImg(idx + 1);
+                }}
+              ></li>
             ))}
           </ul>
         </div>
